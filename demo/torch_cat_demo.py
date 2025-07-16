94f26f6bf4cbee0fd8255d42e9d245c22f6c4a20
Peiran Feng <peiran@fengheasia.com>
2025-07-16 18:03:35 +0800

    delete demos

--- demo/torch_cat_demo.py
+++ demo/torch_cat_demo.py
@@ -6,16 +6,16 @@ import numpy as np
 a = torch.tensor([[1,2,3], [4,5,6], [7,8,9]], dtype=torch.float32, requires_grad=False)
 offset = torch.tensor([10, 10, 10])
 b = torch.cat([a, a+offset, a+offset*2], dim=0)
-print(b)
+print(b) # shape (9, 3)
 b = torch.cat([a, a+offset, a+offset*2], dim=1)
-print(b)
+print(b) # shape (3, 9)
 b = torch.cat([a, a+offset, a+offset*2], dim=-1)
-print(b)
+print(b) # shape (3, 9)
 
 # ndim=3 rank of dim is [-3, 2]
 a = torch.rand((2, 3, 3))
 b = torch.cat([a, a+offset], dim = 2)
-print(b)
+print(b) # shape (2, 3, 6)
 
 # ndim=n rank of dim is [-n, n-1]
 
